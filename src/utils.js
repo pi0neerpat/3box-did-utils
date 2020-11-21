be909e42e4847a03e140ff8bddf613e36335398d
Patrick <blockchainbuddha@gmail.com>
2020-11-20 23:44:44 -0500

    cleanup

--- src/utils.js
+++ src/utils.js
@@ -2,13 +2,7 @@ import { randomBytes } from "@stablelib/random";
 import { DID } from "dids";
 import { Ed25519Provider } from "key-did-provider-ed25519";
 
-// Serverside
-// import { DID } from 'dids'
 import KeyResolver from "@ceramicnetwork/key-did-resolver";
-
-// Resolver
-import Ceramic from "@ceramicnetwork/ceramic-http-client";
-import ThreeIdResolver from "@ceramicnetwork/3id-did-resolver";
 
 //  Client side
 const createDid = async () => {
@@ -16,7 +10,6 @@ const createDid = async () => {
   const provider = new Ed25519Provider(seed);
   const did = new DID({ provider });
   await did.authenticate();
-  // log the DID
   return did;
 };
 
@@ -26,24 +19,13 @@ const signDid = async (did, content) => {
 
 // Serverside
 const verifyJWS = async (jws) => {
-  const did = new DID({ resolver: { registry: KeyResolver.getResolver() } });
+  const did = new DID({
+    resolver: { registry: { ...KeyResolver.getResolver() } },
+  });
   console.log(Object.keys(did));
   console.log(Object.keys(did._resolver));
   console.log(did._resolver.registry);
   await did.verifyJWS(jws); // will throw if the jws is incorrect
 };
-
-// Reolver
-//
-// const ceramic = new Ceramic(<url-of-ceramic-gateway>)
-
-// const registry = {
-// 	...ThreeIdResolver.getResolver(),
-// 	...KeyResolver.getResolver()
-// }
-// const did = new DID({ resolver: { registry } })
-
-// }
-// };
 
 export { createDid, signDid, verifyJWS };
